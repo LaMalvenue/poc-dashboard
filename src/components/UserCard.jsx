1e9da89e082cb4446384859ac94f38f802ee2291
Anaïs Rouvière <anais.rouviere@kobalt.fr>
2020-11-17 11:49:37 +0100

    Ajustements UserCard

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import { CardHeader } from "@material-ui/core";
 import CardContent from "@material-ui/core/CardContent";
 import Card from "@material-ui/core/Card";
-import Avatar from "@material-ui/core/Avatar";
-import IconButton from "@material-ui/core/IconButton";
-import MoreVertIcon from "@material-ui/icons/MoreVert";
 import FormGroup from "@material-ui/core/FormGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
@@ -24,22 +20,6 @@ const UserCard = () => {
 
   return (
     <Card className={classes.root}>
-      <CardHeader
-        avatar={
-          <Avatar
-            alt="Mireille Matthieu avatar"
-            src="https://mk0qomafemocnned3wjh.kinstacdn.com/wp-content/uploads/2019/02/AI-thispersondoesnotexist.jpg"
-            className={classes.large}
-          />
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Julie Fabre"
-        subheader="Formation Développeur web en alternance (Lyon)"
-      />
       <CardContent>
         <FormGroup>
           <FormControlLabel
